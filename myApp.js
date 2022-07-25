052b4c05c805f7c6f85d677cbb78bbe253c867b3
markus.suwignyo <markus.suwignyo@traveloka.com>
2022-07-25 20:55:18 +0700

    refactor / path to send file /views/index.html

--- myApp.js
+++ myApp.js
@@ -3,6 +3,6 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 module.exports = app;
