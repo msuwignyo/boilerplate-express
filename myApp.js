508208202b8ca1e2a3a5c7f6a279145644a80c3d
markus.suwignyo <markus.suwignyo@traveloka.com>
2022-07-25 21:33:56 +0700

    add /now path

--- myApp.js
+++ myApp.js
@@ -23,4 +23,13 @@ app.get("/json", (req, res) => {
   return res.json({ message: "Hello json" });
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => res.json({ time: req.time })
+);
+
 module.exports = app;
