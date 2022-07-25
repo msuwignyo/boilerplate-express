983eccdcd885ad1eb64067c43ecdf76cfe1ccd64
markus.suwignyo <markus.suwignyo@traveloka.com>
2022-07-25 21:19:35 +0700

    add middleware logger

--- myApp.js
+++ myApp.js
@@ -7,6 +7,12 @@ console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  const { method, path, ip } = req;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.get("/json", (req, res) => {
