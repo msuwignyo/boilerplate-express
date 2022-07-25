1ad81d924de7a14f2e00d11b32b71ba5aa9bce07
markus.suwignyo <markus.suwignyo@traveloka.com>
2022-07-25 21:44:21 +0700

    add /name path

--- myApp.js
+++ myApp.js
@@ -34,4 +34,8 @@ app.get(
 
 app.get("/:word/echo", (req, res) => res.json({ echo: req.params.word }));
 
+app.get("/name", (req, res) =>
+  res.json({ name: `${req.query.first} ${req.query.last}` })
+);
+
 module.exports = app;
